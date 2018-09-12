b3f17314de1e05bd08655d30ce9a847a9535fc68
ctorrisdev <charles.torris@gmail.com>
2018-09-12 10:11:34 +0200

    initial commit

---
+++ config/environments/development.php
@@ -0,0 +1,8 @@
+<?php
+/** Development */
+define('SAVEQUERIES', true);
+define('WP_DEBUG', true);
+define('SCRIPT_DEBUG', true);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);

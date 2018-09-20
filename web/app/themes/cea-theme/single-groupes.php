080215ddcc357fdd06a62ebe60de17d29db57dde
ctorrisdev <charles.torris@gmail.com>
2018-09-20 11:33:12 +0200

    update

---
+++ web/app/themes/cea-theme/single-groupes.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * 
+ * @package WordPress
+ * @subpackage cea
+ */
+acf_form_head();
+get_header();
+?>
+<?php while (have_posts()) : the_post(); ?>
+
+   
+ <?php display_wall(get_the_id()); ?>
+
+<?php endwhile; ?>
+<?php get_footer(); ?>

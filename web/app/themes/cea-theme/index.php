74d1c3276ceabc4f01776eac8cdcf899c5724f6c
Charles <charles@51-15-181-30.online.net>
2018-09-20 11:29:30 +0200

    post crash update

--- web/app/themes/cea-theme/index.php
+++ web/app/themes/cea-theme/index.php
@@ -8,51 +8,19 @@
  * E.g., it puts together the home page when no home.php file exists.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package cea
+ * 
+ * @package WordPress
+ * @subpackage cea
  */
+acf_form_head();
+get_header(); ?>
 
-get_header();
-?>
+<?php while(have_posts()) : the_post(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	title: <?php the_title(); ?><br />
+	<?php the_content(); ?>
+	<hr />
 
-		<?php
-		if ( have_posts() ) :
+<?php endwhile; ?>
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_footer();
+<?php get_footer(); ?>
--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 /** WordPress view bootstrapper */
 define('WP_USE_THEMES', true);
 require __DIR__ . '/wp/wp-blog-header.php';
